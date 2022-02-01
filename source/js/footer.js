cd5a2c932cc43bd765a753d628d0ddbe9138e401
Stillrain577681223 <95129294+Stillrain577681223@users.noreply.github.com>
2022-02-01 21:34:32 +0800

    Add files via upload

---
+++ source/js/footer.js
@@ -0,0 +1,10 @@
+document.write('<!-----------  footer----------->');
+document.write('' +
+    '<center>\n' +
+    '<p style="color:#333;">© 2022 Wang Li and her parents</p>\n' +
+    '</center>\n' +
+    '');
+
+
+
+
